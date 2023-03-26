c1f264bb8dd7d4c03d7691602dcc0f5d4bcadbb0
IvoMicisin <micisin.ivo@gmail.com>
2023-03-27 01:48:09 +0200

    vela porobeno = chod do tabulky a uvidis

---
+++ htdocs/wwwroot/js/HBr.js
@@ -0,0 +1,88 @@
+var camera, scene, renderer, controls;
+var cube, HBr;
+var spotlight;
+
+function addLights() {
+  ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+  scene.add(ambientLight);
+
+  var pointLight1 = new THREE.PointLight(0xffffff, 3, 10);
+  pointLight1.position.set(2, 3, 3);
+  pointLight1.castShadow = true;
+  scene.add(pointLight1);
+
+  var pointLight2 = new THREE.PointLight(0xffffff, 1, 15);
+  pointLight2.position.set(-5, -2, 3);
+  pointLight2.castShadow = true;
+  scene.add(pointLight2);
+
+  var pointLight3 = new THREE.PointLight(0xffffff, 2, 20);
+  pointLight3.position.set(-1,1,-5);
+  pointLight3.castShadow = true;
+  scene.add(pointLight3);
+
+  //var helper1 = new THREE.PointLightHelper(pointLight1,1);
+  //scene.add(helper1)
+  //var helper2 = new THREE.PointLightHelper(pointLight2,1);
+  //scene.add(helper2)
+  //var helper3 = new THREE.PointLightHelper(pointLight3,1);
+  //scene.add(helper3)
+}
+
+function addHBr() {
+  const loader = new THREE.GLTFLoader();
+  loader.load("/htdocs/models/1_rocnik/kyseliny/bezkyslikate/jednosytne/HBr.glb", function (imported_model) {
+    scene.add(imported_model.scene);
+    HBr = imported_model.scene;
+    HBr.position.set(0, 0, 0);
+    HBr.rotation.set(0, 4.75, 0)
+    HBr.castShadow = true;
+  });
+}
+
+function addSphere() {
+  var geometry = new THREE.SphereGeometry(50, 50, 50);
+  var material = new THREE.MeshPhongMaterial({
+    color: 0x43425c,
+    side: THREE.DoubleSide,
+    reflectivity: 0.12,
+    shininess: 0.65,
+  });
+  sphere = new THREE.Mesh(geometry, material);
+  sphere.recieveShadow = true;
+  scene.add(sphere);
+}
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    1000
+  );
+  camera.position.set(0, 0, 5);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  document.body.appendChild(renderer.domElement);
+
+  scene = new THREE.Scene();
+  addLights();
+  addSphere()
+  addHBr();
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+}
+
+function render() {
+  requestAnimationFrame(render);
+
+  renderer.render(scene, camera);
+  camera.lookAt(scene.position);
+}
+
+
+init();
+render();
